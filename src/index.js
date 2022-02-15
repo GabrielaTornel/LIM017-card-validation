3ffbadca9450e4972a6f93524134e6002c0a3267
gabriela tornel <tornelgabriela@gmail.com>
2022-02-15 11:27:50 -0500

    revertir array en funcion isValid

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-import validator from './validator.js';
+import validator from "./validator.js";
 
 console.log(validator);
+console.log(validator.isValid("445852"))
+
+/*const donacionBtn = document.querySelector("#donacion");
+donacionBtn.addEventListener("click", () =>{
+    const inputValue = document.getElementById(donation)
+})
+function donation(){
+    document.getElementById("Screen2").className = "visible"; 
+    document.getElementById("Screen1").className = "invisible";
+}*/
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,26 @@
 const validator = {
-  // ...
-};
+  isValid: function (creditCardNumber){
+    console.log(creditCardNumber)
+    //declarar arreglo tarjeta formulario
+    let array= Array.from(creditCardNumber);
+    console.log(array)
+    //reversa de mi array
+    const reverse= array.reverse()
+    console.log(reverse)
+    //declarar arreglo para los pares
+    let arrayP= [];
+   //
+    let sum= 0;
+    // declarar 
+    for (let i=0; i<=array.length; i++){
+      arrayP.push(parseInt(array[i]));
+    }
+    //hay que 
+   /* arrayP.reverse();
+    for (let i=0; i<arrayP.length; i++){
+      if ((i+1)%2 ==0)
+    }*/
 
+  }
+}
 export default validator;
