d84614ded4acf03ab4b6359df14c18e243bfded5
gabriela tornel <tornelgabriela@gmail.com>
2022-02-16 17:47:17 -0500

    convertir string en numeros

--- src/index.js
+++ src/index.js
@@ -1,13 +1,32 @@
 import validator from "./validator.js";
+//click para cambiar de pagina
 
+//Funciene el boton de validacion 
+const checkCreditNumber = document.getElementById("check");
+checkCreditNumber.addEventListener("click", validarCard);
+console.log(checkCreditNumber);
+
+function validarCard(){
+let creditCardNumber = document.getElementById("creditCardNumber").value;
+if (creditCardNumber == ""){
+    alert("ingresa numero de tarjeta");
+}
+else {
+    //alert ("tarjeta invalida");
+    validator.isValid(creditCardNumber)
+}
+}
+console.log(validarCard);
 console.log(validator);
-console.log(validator.isValid("445852"))
+console.log(validator.isValid);
+/* console.log("click donacion");
+let donacion = document.getElementById("donacion");
+donacion.addEventListener("click", clickDonacion);
 
-/*const donacionBtn = document.querySelector("#donacion");
-donacionBtn.addEventListener("click", () =>{
-    const inputValue = document.getElementById(donation)
-})
-function donation(){
-    document.getElementById("Screen2").className = "visible"; 
-    document.getElementById("Screen1").className = "invisible";
-}*/
+let check = document.getElementById("check");
+check.addEventListener("click",validar);
+
+function validar() {
+  alert("ingrese tajeta")
+    }
+*/
--- src/validator.js
+++ src/validator.js
@@ -7,20 +7,19 @@ const validator = {
     //reversa de mi array
     const reverse= array.reverse()
     console.log(reverse)
-    //declarar arreglo para los pares
-    let arrayP= [];
-   //
-    let sum= 0;
-    // declarar 
-    for (let i=0; i<=array.length; i++){
-      arrayP.push(parseInt(array[i]));
+    //string a numeros
+    let arrayDos= [];
+    for(let i=0; i<array.length; i++){
+      arrayDos.push(parseInt(array[i]));
+      console.log(arrayDos)
     }
-    //hay que 
-   /* arrayP.reverse();
-    for (let i=0; i<arrayP.length; i++){
-      if ((i+1)%2 ==0)
-    }*/
-
-  }
+   //declarar arreglo para los pares
+   
+   // multiplicar los segundos x2
+   //Si la multiplicación es mayor q 9 sumar los digitos (darle una funcionalidad) 
+   //sumar el array final 
+   //dividir entre 2 
+   //if ((i+1)%2 ==0)
+    }
 }
 export default validator;
